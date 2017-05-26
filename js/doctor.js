854b82eb20197f4300d3d98cd6cac110145486fb
Brian Fan <kuomu.fan@yahoo.com>
2017-05-26 11:50:37 -0700

    change aki request link to narrow the search location to Portland, OR

--- js/doctor.js
+++ js/doctor.js
@@ -5,7 +5,7 @@ function Doctor(){
 }
 
 Doctor.prototype.getDoctor = function(symptom, displayDoctor) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + symptom + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + symptom + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(response){
     displayDoctor(response);
   })
