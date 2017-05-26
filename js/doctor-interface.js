3e15081609bd80d76f0119cbfc6441c219f7aa6c
Brian Fan <kuomu.fan@yahoo.com>
2017-05-26 10:55:13 -0700

    display name, specialty and description of doctor on webpage

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,7 +1,29 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-var displayDoctor = function(reponse){
-  
+var displayDoctor = function(response){
+  response.data.forEach(function(doctor){
+    console.log(doctor);
+    var image_url = doctor.profile.image_url;
+    var name = doctor.profile.first_name + ' ' + doctor.profile.last_name + ' ' + doctor.profile.title;
+    console.log(name);
+    var specialty = doctor.specialties[0].actor;
+    console.log(specialty);
+    var description = doctor.specialties[0].description;
+    console.log(description);
+
+    var display =
+      '<div class="col-md-4 mb-3">' +
+      '<div class="card">' +
+        '<img class="card-img-top" src="' + image_url + '" alt="Card image cap" style="height: 250px;">' +
+        '<div class="card-block">' +
+          '<h4 class="card-title mb-3">' + name + '</h4>' +
+          "<p><b>Specialty: </b> " + specialty + "</p>" +
+          "<p><b>Description: :</b>" + description + "</p>" +
+        '</div>' +
+      '</div>' +
+    '</div>';
+    $('.result').append(display);
+  });
 };
 
 $(document).ready(function(){
@@ -10,7 +32,6 @@ $(document).ready(function(){
   $('.find-doctor').click(function(){
     $('.result').text('');
     var symptom = $('#symptom').val();
-    console.log(symptom);
     $('#symptom').val("");
 
     doctorObject.getDoctor(symptom, displayDoctor);
--- js/doctor.js
+++ js/doctor.js
@@ -7,7 +7,6 @@ function Doctor(){
 Doctor.prototype.getDoctor = function(symptom, displayDoctor) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + symptom + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey)
   .then(function(response){
-    console.log(response);
     displayDoctor(response);
   })
   .fail(function(error) {
